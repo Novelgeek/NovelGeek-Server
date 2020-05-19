b5aed5e430d66eb1905c474bd703b3410fe30063
Dhanushka Samarasinghe <dhanushkahere@gmail.com>
2020-05-19 16:25:11 +0530

    first commit

---
+++ src/main/java/lk/ucsc/NovelGeek/NovelGeekApplication.java
@@ -0,0 +1,13 @@
+package lk.ucsc.NovelGeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelGeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelGeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ucsc/NovelGeek/NovelGeekApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ucsc.NovelGeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelGeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
