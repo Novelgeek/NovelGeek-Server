88d7d4cba8d526795069113ae7a418736b2c8e90
kiruthiharan <kiruthiharan@gmail.com>
2020-06-21 22:14:03 +0530

    upload img filename

--- src/main/java/lk/ucsc/NovelGeek/controller/AWSS3Ctrl.java
+++ src/main/java/lk/ucsc/NovelGeek/controller/AWSS3Ctrl.java
@@ -4,11 +4,10 @@ import lk.ucsc.NovelGeek.service.AWSS3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(value= "/s3")
@@ -17,10 +16,19 @@ public class AWSS3Ctrl {
     @Autowired
     private AWSS3Service service;
 
+//    @PostMapping(value= "/upload")
+//    public ResponseEntity<String> uploadFile(@RequestPart(value= "file") final MultipartFile multipartFile) {
+//        //service.uploadFile(multipartFile);
+//        System.out.println(payload.get("boom"));
+//        final String response = "[" + multipartFile.getOriginalFilename() + "] uploaded successfully.";
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//    }
+
     @PostMapping(value= "/upload")
-    public ResponseEntity<String> uploadFile(@RequestPart(value= "file") final MultipartFile multipartFile) {
-        service.uploadFile(multipartFile);
-        final String response = "[" + multipartFile.getOriginalFilename() + "] uploaded successfully.";
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public ResponseEntity<String> uploadFile2(@RequestParam("boom") String model, @RequestParam(value = "file", required = false) MultipartFile multipartFile) {
+        String fileUrl = service.uploadFile(multipartFile);
+//        System.out.println(model);
+        //final String response = "[" + multipartFile.getOriginalFilename() + "] uploaded successfully.";
+        return new ResponseEntity<>(fileUrl, HttpStatus.OK);
     }
 }
--- src/main/java/lk/ucsc/NovelGeek/service/AWSS3Service.java
+++ src/main/java/lk/ucsc/NovelGeek/service/AWSS3Service.java
@@ -3,8 +3,9 @@ package lk.ucsc.NovelGeek.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.util.Date;
 
+import com.amazonaws.services.s3.model.PutObjectResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,21 +27,25 @@ public class AWSS3Service  {
     private AmazonS3 amazonS3;
     @Value("${amazonProperties.bucketName}")
     private String bucketName;
+    @Value("${amazonProperties.endpointUrl}")
+    private String endPointUrl;
 
     // @Async annotation ensures that the method is executed in a different background thread
     // but not consume the main thread.
-    @Async
-    public void uploadFile(final MultipartFile multipartFile) {
+//    @Async
+    public String uploadFile(final MultipartFile multipartFile) {
         LOGGER.info("File upload in progress.");
         try {
             final File file = convertMultiPartFileToFile(multipartFile);
-            uploadFileToS3Bucket(bucketName, file);
+            String fileName = uploadFileToS3Bucket(bucketName, file);
             LOGGER.info("File upload is completed.");
             file.delete();  // To remove the file locally created in the project folder.
+            return endPointUrl + "/" + fileName;
         } catch (final AmazonServiceException ex) {
             LOGGER.info("File upload is failed.");
             LOGGER.error("Error= {} while uploading file.", ex.getMessage());
         }
+        return null;
     }
 
     private File convertMultiPartFileToFile(final MultipartFile multipartFile) {
@@ -53,10 +58,11 @@ public class AWSS3Service  {
         return file;
     }
 
-    private void uploadFileToS3Bucket(final String bucketName, final File file) {
-        final String uniqueFileName = LocalDateTime.now() + "_" + file.getName();
+    private String uploadFileToS3Bucket(final String bucketName, final File file) {
+        final String uniqueFileName = new Date().getTime() + "_" + file.getName().replace(" ", "_");
         LOGGER.info("Uploading file with name= " + uniqueFileName);
         final PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, uniqueFileName, file);
-        amazonS3.putObject(putObjectRequest);
+        PutObjectResult result= amazonS3.putObject(putObjectRequest);
+        return uniqueFileName;
     }
 }
