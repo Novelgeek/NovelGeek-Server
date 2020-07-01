91d8bf35f592d14099942f3875232417bdd34421
Dhanushka Samarasinghe <dhanushkahere@gmail.com>
2020-07-01 23:08:48 +0530

    before production

--- src/main/java/lk/ucsc/NovelGeek/controller/UserController.java
+++ src/main/java/lk/ucsc/NovelGeek/controller/UserController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
     @PostMapping(path="/changeName",consumes = "applicatoin/json",produces = "application/json")
     public void changeUserName(){
-        System.out.println("came to server");
+        System.out.println("came to serverr ");
     }
 }
