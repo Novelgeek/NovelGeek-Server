acf627d466bbf2ea65df6b67a89dd81c6a96005a
archana1996 <archanaahil@gmail.com.com>
2020-07-30 23:09:07 +0530

    getting users for chat

--- src/main/java/lk/ucsc/NovelGeek/controller/UserController.java
+++ src/main/java/lk/ucsc/NovelGeek/controller/UserController.java
@@ -44,4 +44,9 @@ public class UserController {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllUsersExceptMe(){
+        return ResponseEntity.ok(userService.getAllUsersExceptMe());
+    }
+
 }
